78a74a1af324bea4aa1fede3b57c997b18da5163
rivelynooliveira <rivelynooliveira@gmail.com>
2021-01-19 20:27:02 -0300

    first commit

---
+++ src/main/java/natan/io/projeto1/entity/User.java
@@ -0,0 +1,43 @@
+package natan.io.projeto1.entity;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String email;
+	private String name;
+	
+	@ManyToMany
+	private Set<Role> roles;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
