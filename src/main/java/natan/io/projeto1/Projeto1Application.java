c5989af4a885ad3bacf8dce51679abc340a14fad
rivelynooliveira <rivelynooliveira@gmail.com>
2021-01-22 20:17:10 -0300

    Usando MVC
    Adicionando MVC para exibição de usuários

--- src/main/java/natan/io/projeto1/Projeto1Application.java
+++ src/main/java/natan/io/projeto1/Projeto1Application.java
@@ -2,8 +2,10 @@ package natan.io.projeto1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "natan.io.*")
 public class Projeto1Application {
 
 	public static void main(String[] args) {
