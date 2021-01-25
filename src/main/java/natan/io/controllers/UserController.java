8b97d293b96dee68fa7f6a77345c5e59a1b26a3a
rivelynooliveira <rivelynooliveira@gmail.com>
2021-01-25 20:07:50 -0300

    Usando injeção de dependência

--- src/main/java/natan/io/controllers/UserController.java
+++ src/main/java/natan/io/controllers/UserController.java
@@ -1,23 +1,22 @@
 package natan.io.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import natan.io.projeto1.repository.UserRepository;
+import natan.io.service.UserService;
 
 @Controller
 public class UserController {
 	
-	private UserRepository userRepository;
-
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/user")
 	public String getUsers(Model model) {
-		model.addAttribute("users", this.userRepository.findAll());
+		model.addAttribute("users", this.userService.findAll());
 		
 		return "user";
 	}
